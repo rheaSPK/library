66872f0d102d40397ec0d1d93741d6d1d9875cfb
simon-pierrekrautkramer <skraemer60@gmail.com>
2022-09-17 14:49:17 +0200

    add delete button

--- scripts.js
+++ scripts.js
@@ -14,10 +14,21 @@ function addBookToLibrary(book) {
     myLibrary.push(book)
 }
 
+function deleteBook(id){
+    myLibrary.splice(id, id+1)
+}
+
 function displayBook(){
-    let table = `<table class="books"><tr><th>Title</th><th>Author</th><th>Pages</th><th>Read?</th></tr>`
+    let table = `<table class="books"><tr><th>Title</th><th>Author</th><th>Pages</th><th>Read?</th><th></th></tr>`
     for(let book in myLibrary){
-        const rowHtml = `<tr><td>${myLibrary[book].title}</td><td>${myLibrary[book].author}</td><td>${myLibrary[book].pages}</td><td>${myLibrary[book].read ? "already read." : "not read yet."}</td></tr>`
+        const rowHtml = 
+            `<tr>
+                <td>${myLibrary[book].title}</td>
+                <td>${myLibrary[book].author}</td>
+                <td>${myLibrary[book].pages}</td>
+                <td>${myLibrary[book].read ? "already read." : "not read yet."}</td>
+                <td><button type="button" class="delete-book" id="${book}" onclick="removeBookFromTable(${book})")>Delete</button></td>
+            </tr>`
         table += rowHtml
     }
     table += "</table>"
