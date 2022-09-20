ca393399d6236053cebb43c09d4e54552299070e
simon-pierrekrautkramer <skraemer60@gmail.com>
2022-09-20 15:53:03 +0200

    rewrote from function to class

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 let myLibrary = [];
+let myBookClasLibrary = []
 
-function Book(title, author, pages, read){
+function functionBook(title, author, pages, read){
     this.title = title
     this.author = author
     this.pages = pages
@@ -18,7 +19,68 @@ function deleteBook(id){
     myLibrary.splice(id, id+1)
 }
 
-function displayBook(){
+class Book{
+    #title
+    #author
+    #pages
+    #read
+
+    constructor(title, author, pages, read){
+        this.#title = title
+        this.#author = author
+        this.#pages = pages
+        this.#read = read 
+    }
+
+    get title() {
+        return this.#title
+    }
+
+    get author() {
+        return this.#author
+    }
+
+    get pages() {
+        return this.#pages
+    }
+
+    get read() {
+        return this.#read
+    }
+
+    set read(value) {
+        // if (typeof(value) == "boolean"){
+        //     this.#read = value
+        //     return true
+        // }
+        // return false
+        this.#read = value
+    }
+
+    readText(){
+        return this.#read  ? "already read" : "not read yet"
+    }
+}
+
+function displayBook() {
+    let table = `<table class="books"><tr><th>Title</th><th>Author</th><th>Pages</th><th>Read?</th><th></th></tr>`
+    for(let book in myLibrary){
+        const rowHtml = 
+            `<tr>
+                <td>${myLibrary[book].title}</td>
+                <td>${myLibrary[book].author}</td>
+                <td>${myLibrary[book].pages}</td>
+                <td><button type="button" class="toggle-read" id="${book}" onclick="toggleRead(${book})">${myLibrary[book].readText()}</button></td>
+                <td><button type="button" class="delete-book" id="${book}" onclick="removeBookFromTable(${book})")>Delete</button></td>
+            </tr>`
+        table += rowHtml
+    }
+    table += "</table>"
+    return table 
+}
+
+
+function displayFunctionBook(){
     let table = `<table class="books"><tr><th>Title</th><th>Author</th><th>Pages</th><th>Read?</th><th></th></tr>`
     for(let book in myLibrary){
         const rowHtml = 
